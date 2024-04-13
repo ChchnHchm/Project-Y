584d599f94db7c2efe81c4afe445413de91f3268
HomiechanChuh <hichem.chouchane@etu.u-bordeaux.fr>
2024-04-13 21:34:56 +0200

    add weapon sway

--- Item/Weapon/Weapons.gd
+++ Item/Weapon/Weapons.gd
@@ -2,9 +2,15 @@ class_name Weapons extends Resource
 
 @export var name : StringName
 @export_category("Weapon Orientation")
-@export var positionRightHand : Vector3
-@export var positionLeftHand : Vector3
+@export var position : Vector3
 @export var rotation : Vector3
+@export_category("Weapon Sway")
+@export var SwayMin: Vector2 = Vector2(-20.0,-20.0)
+@export var SwayMax : Vector2 = Vector2(20.0,20.0)
+@export_range(0,0.2,0.01) var SwaySpeedPosition : float = 0.07
+@export_range(0,0.2,0.01) var SwaySpeedRotation : float = 0.1
+@export_range(0,0.25,0.01) var SwayAmountPosition : float = 0.1
+@export_range(0,50,0.1) var SwayAmountRotation : float = 30
 @export_category("Visual Settings")
 @export var mesh : Mesh
 @export var shadow : bool
--- Scenes/InitWeapon.gd
+++ Scenes/InitWeapon.gd
@@ -10,12 +10,13 @@ extends Node3D
 @onready var weaponMesh : MeshInstance3D = $WeaponMesh
 @onready var weaponShadow : MeshInstance3D = $WeaponShadow
 
+var MouseMovement : Vector2
 func _input(event):
 	if event.is_action_pressed("Switch Hand"):
-		if position == WeaponType.positionRightHand:
-			position = WeaponType.positionLeftHand
-		else:
-			position = WeaponType.positionRightHand
+		position.x =  - position.x
+	if event is InputEventMouseMotion:
+		MouseMovement = event.relative
+		
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -23,6 +24,27 @@ func _ready():
 
 func loadWeapon() -> void:
 	weaponMesh.mesh = WeaponType.mesh
-	position = WeaponType.positionRightHand
+	position = WeaponType.position
 	rotation_degrees = WeaponType.rotation
 	weaponShadow.visible = WeaponType.shadow
+
+func SwayWeapon(delta) -> void:
+	
+	#clamp mouse movement
+	MouseMovement = MouseMovement.clamp(WeaponType.SwayMin,WeaponType.SwayMax)
+	
+	#lerp position
+	var WeaponTypePosX : float = WeaponType.position.x
+	if position.x < 0:
+		# we take neg value if left handed
+		WeaponTypePosX = - WeaponTypePosX
+	
+	position.x = lerp(position.x, WeaponTypePosX - (MouseMovement.x * WeaponType.SwayAmountPosition) * delta, WeaponType.SwaySpeedPosition)
+	position.y = lerp(position.y, WeaponType.position.y + (MouseMovement.y * WeaponType.SwayAmountPosition) * delta, WeaponType.SwaySpeedPosition)
+
+	#lerp rotation
+	rotation_degrees.x = lerp(rotation_degrees.x, WeaponType.rotation.x - (MouseMovement.y * WeaponType.SwayAmountRotation) * delta, WeaponType.SwaySpeedRotation)
+	rotation_degrees.y = lerp(rotation_degrees.y, WeaponType.rotation.y + (MouseMovement.x * WeaponType.SwayAmountRotation) * delta, WeaponType.SwaySpeedRotation)
+	
+func _physics_process(delta):
+	SwayWeapon(delta)
