e81463c613a80d2a764f39e24df6de79625c44fd
HomiechanChuh <hichem.chouchane@etu.u-bordeaux.fr>
2024-04-14 13:06:03 +0200

    add weapon idle animation

--- Item/Weapon/Weapons.gd
+++ Item/Weapon/Weapons.gd
@@ -4,6 +4,7 @@ class_name Weapons extends Resource
 @export_category("Weapon Orientation")
 @export var position : Vector3
 @export var rotation : Vector3
+@export var scale : Vector3
 @export_category("Weapon Sway")
 @export var SwayMin: Vector2 = Vector2(-20.0,-20.0)
 @export var SwayMax : Vector2 = Vector2(20.0,20.0)
@@ -11,6 +12,9 @@ class_name Weapons extends Resource
 @export_range(0,0.2,0.01) var SwaySpeedRotation : float = 0.1
 @export_range(0,0.25,0.01) var SwayAmountPosition : float = 0.1
 @export_range(0,50,0.1) var SwayAmountRotation : float = 30
+@export var IdleSwayAdjustement : float = 10.0
+@export var IdleSwayRotationStrenght : float = 300.0
+@export_range(0.1,10.0,0.1) var RandomSwayAmount : float = 5.0
 @export_category("Visual Settings")
 @export var mesh : Mesh
 @export var shadow : bool
--- Scenes/InitWeapon.gd
+++ Scenes/InitWeapon.gd
@@ -9,6 +9,22 @@ extends Node3D
 
 @onready var weaponMesh : MeshInstance3D = $WeaponMesh
 @onready var weaponShadow : MeshInstance3D = $WeaponShadow
+@export var player : Player
+@export var sway_noise : NoiseTexture2D
+@export var sway_speed : float = 1.2
+@export var reset : bool = false:
+	set(value):
+		reset = value
+		if Engine.is_editor_hint():
+			loadWeapon()
+var RandomSwayX
+var RandomSwayY
+var RandomSwayAmount : float
+var time : float = 0.0
+var IdleSwayAdjustement
+var IdleSwayRotationStrength
+
+
 
 var MouseMovement : Vector2
 func _input(event):
@@ -27,24 +43,45 @@ func loadWeapon() -> void:
 	position = WeaponType.position
 	rotation_degrees = WeaponType.rotation
 	weaponShadow.visible = WeaponType.shadow
+	scale = WeaponType.scale
+	IdleSwayAdjustement = WeaponType.IdleSwayAdjustement
+	IdleSwayRotationStrength = WeaponType.IdleSwayRotationStrenght
+	RandomSwayAmount = WeaponType.RandomSwayAmount
 
 func SwayWeapon(delta) -> void:
-	
-	#clamp mouse movement
-	MouseMovement = MouseMovement.clamp(WeaponType.SwayMin,WeaponType.SwayMax)
-	
-	#lerp position
-	var WeaponTypePosX : float = WeaponType.position.x
-	if position.x < 0:
-		# we take neg value if left handed
-		WeaponTypePosX = - WeaponTypePosX
-	
-	position.x = lerp(position.x, WeaponTypePosX - (MouseMovement.x * WeaponType.SwayAmountPosition) * delta, WeaponType.SwaySpeedPosition)
-	position.y = lerp(position.y, WeaponType.position.y + (MouseMovement.y * WeaponType.SwayAmountPosition) * delta, WeaponType.SwaySpeedPosition)
+	if not Engine.is_editor_hint():
+		#Get random sway value
+		var SwayRandom : float = GetSwayNoise()
+		var SwayRandomAdjusted : float = SwayRandom * IdleSwayAdjustement
+		
+		#create time with delta
+		time += delta * (sway_speed + SwayRandom)
+		RandomSwayX = sin(time * 1.5 + SwayRandomAdjusted) / RandomSwayAmount
+		RandomSwayY = sin(time - SwayRandomAdjusted) / RandomSwayAmount
+		
+		#clamp mouse movement
+		MouseMovement = MouseMovement.clamp(WeaponType.SwayMin,WeaponType.SwayMax)
+		
+		#lerp position
+		var WeaponTypePosX : float = WeaponType.position.x
+		if position.x < 0:
+			# we take neg value if left handed
+			WeaponTypePosX = - WeaponTypePosX
+		
+		position.x = lerp(position.x, WeaponTypePosX - (MouseMovement.x * WeaponType.SwayAmountPosition + RandomSwayX) * delta, WeaponType.SwaySpeedPosition)
+		position.y = lerp(position.y, WeaponType.position.y + (MouseMovement.y * WeaponType.SwayAmountPosition + RandomSwayY) * delta, WeaponType.SwaySpeedPosition)
 
-	#lerp rotation
-	rotation_degrees.x = lerp(rotation_degrees.x, WeaponType.rotation.x - (MouseMovement.y * WeaponType.SwayAmountRotation) * delta, WeaponType.SwaySpeedRotation)
-	rotation_degrees.y = lerp(rotation_degrees.y, WeaponType.rotation.y + (MouseMovement.x * WeaponType.SwayAmountRotation) * delta, WeaponType.SwaySpeedRotation)
+		#lerp rotation
+		rotation_degrees.x = lerp(rotation_degrees.x, WeaponType.rotation.x - (MouseMovement.y * WeaponType.SwayAmountRotation + (RandomSwayX * IdleSwayRotationStrength)) * delta, WeaponType.SwaySpeedRotation)
+		rotation_degrees.y = lerp(rotation_degrees.y, WeaponType.rotation.y + (MouseMovement.x * WeaponType.SwayAmountRotation + (RandomSwayY * IdleSwayRotationStrength)) * delta, WeaponType.SwaySpeedRotation)
 	
 func _physics_process(delta):
 	SwayWeapon(delta)
+
+func GetSwayNoise() -> float:
+	var PlayerPosition : Vector3 = Vector3(0,0,0)
+	
+	if not Engine.is_editor_hint():
+		PlayerPosition = player.global_position
+	
+	return sway_noise.noise.get_noise_2d(PlayerPosition.x,PlayerPosition.y)
